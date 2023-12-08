4a47c55aadf3e41ee36836535cc291db1de7269f
nassimoyah <107773569+nassimoyah@users.noreply.github.com>
2023-12-09 00:57:36 +0100

    Create resize.py

---
+++ resize.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+
+img = cv.imread('C:\\Users\\pc cam\\Desktop\\rock\\1.png')
+
+cv.imshow('hand',img)
+
+def resc(frame,scale=0.75): 
+    width = int( frame.shape[1]  * scale )
+    hight = int( frame.shape[0]  * scale )
+    dim = (width,hight)
+    return cv.resize(frame,dim,interpolation = cv.INTER_AREA)
+rf = resc(img)
+cv.imshow('hand2',rf)
+cv.waitKey(0)
