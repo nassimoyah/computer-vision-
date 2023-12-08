e19bbb4bc944d7f998efd5a88c82d195a7a0b906
nassimoyah <107773569+nassimoyah@users.noreply.github.com>
2023-12-09 00:58:46 +0100

    Update resize.py

--- resize.py
+++ resize.py
@@ -8,7 +8,7 @@ def resc(frame,scale=0.75):
     width = int( frame.shape[1]  * scale )
     hight = int( frame.shape[0]  * scale )
     dim = (width,hight)
-    return cv.resize(frame,dim,interpolation = cv.INTER_AREA)
+    return cv.resize(frame,dim)
 rf = resc(img)
 cv.imshow('hand2',rf)
 cv.waitKey(0)
