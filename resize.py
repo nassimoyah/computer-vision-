ac57593078ebc9de820693fa982f5e139ba716a2
nassimoyah <107773569+nassimoyah@users.noreply.github.com>
2023-12-09 13:26:02 +0100

    Update resize.py

--- resize.py
+++ resize.py
@@ -1,14 +1,14 @@
 import cv2 as cv
 
 img = cv.imread('C:\\Users\\pc cam\\Desktop\\rock\\1.png')
-
 cv.imshow('hand',img)
 
-def resc(frame,scale=0.75): 
+def resize(frame,scale=0.75): 
     width = int( frame.shape[1]  * scale )
     hight = int( frame.shape[0]  * scale )
     dim = (width,hight)
     return cv.resize(frame,dim)
-rf = resc(img)
-cv.imshow('hand2',rf)
+    
+resized_imag=  resize(img)
+cv.imshow('hand2',resized_imag)
 cv.waitKey(0)
